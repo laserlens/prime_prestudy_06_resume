fe26caff5179a08c903ded25fac2e618113f95d7
laserlens <laserlens@gmail.com>
2016-10-27 11:27:09 -0500

    syntax clean up

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("#show").click(function(){
-    $("#page-wrap").removeClass("hidden"); });
+    $("#page-wrap").removeClass("hidden");
+    $("#show").remove();
+  });
  });
- 
